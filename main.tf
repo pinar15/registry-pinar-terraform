6f552a6d335f754ec9e99f3dfe894769ec63cd63
pinar15 <pdasdemir49@gmail.com>
2023-02-27 19:54:10 -0600

    message

---
+++ main.tf
@@ -0,0 +1,17 @@
+module my-repository {
+    source = "./module"
+    artifact-config = {
+        repository_id = "my-repo"
+        location = "us-central1"
+
+    }
+}
+
+module nodejs-repository {
+    source = "./module"
+    artifact-config = {
+        repository_id = "nodejs-repo"
+        location = "us-central1"
+
+    }
+}
---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "google" {
+  project = "glowing-market-377017"
+  region  = "us-central1"
+}
